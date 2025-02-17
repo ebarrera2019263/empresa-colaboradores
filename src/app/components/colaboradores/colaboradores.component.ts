2e0e2589538c02156ff7228a5ccc0c33a20c9466
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2025-02-17 16:28:26 -0600

    se agregaron componentes y servicios

---
+++ src/app/components/colaboradores/colaboradores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colaboradores',
+  imports: [],
+  templateUrl: './colaboradores.component.html',
+  styleUrl: './colaboradores.component.css'
+})
+export class ColaboradoresComponent {
+
+}
